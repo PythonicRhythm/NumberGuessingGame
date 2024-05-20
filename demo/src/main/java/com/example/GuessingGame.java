4a0cfbd0e02ee21d76e8d619a4c99aa271874c4c
Anthony Alvarez <101856161+PythonicRhythm@users.noreply.github.com>
2024-05-20 15:57:14 -0500

    Input Validation + commenting + full loop functionality.

--- demo/src/main/java/com/example/GuessingGame.java
+++ demo/src/main/java/com/example/GuessingGame.java
@@ -1,36 +1,59 @@
 package com.example;
 import java.util.Scanner;
 
+/*
+ * The GuessingGame class' only purpose is to connect to whichever guessing game mode
+ * you want to play. TimedGuessing, ClassicGuessing.java, and DifficultyGuessing.java
+ * can be called statically via it's run method and this GuessingGame.java class
+ * serves as a gateway to those three modes.
+ */
 public class GuessingGame {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to my Guessing Game Collection!");
-        System.out.println("For Classic Mode type 1\nFor Difficulty Modes type 2\nFor Timed Mode type 3\nTo Exit type 0");
+        Scanner reader = new Scanner(System.in);
         while(true) {
+            System.out.println("\nWelcome to my Guessing Game Collection!");
+            System.out.println("For Classic Mode type 1\nFor Difficulty Modes type 2\nFor Timed Mode type 3\nTo Exit type 0");
             System.out.print("> ");
-            Scanner reader = new Scanner(System.in);
-            int choice = Integer.parseInt(reader.nextLine());
 
+            // Confirm that user input is not a string.
+            int choice;
+            try {
+                choice = Integer.parseInt(reader.nextLine());
+            } catch (NumberFormatException ex) {
+                System.out.println("Invalid Input. Please input a number from 0-3.");
+                continue;
+            }
+
+            // Exiting
             if(choice == 0) {
                 System.out.println("Goodbye!");
                 reader.close();
                 System.exit(0);
             }
+            // Classic Mode
             else if(choice == 1)
             {
                 System.out.println("Launching Classic Mode...");
                 ClassicGuessing.run();
+                
             }
+            // Difficulty Modes
             else if(choice == 2)
             {
                 System.out.println("Launching Difficulty Modes...");
                 DifficultyGuessing.run();
+                
+
             }
+            // Timed Mode
             else if(choice == 3)
             {
                 System.out.println("Launching Timed Mode...");
                 TimedGuessing.run();
+                
             }
+            // Invalid response
             else {
                 System.out.println("Invalid response. Enter a number from 0-3.");
             }
