b7f766d88131e6fbd5a1333d632c0980981d8046
Anthony Alvarez <101856161+PythonicRhythm@users.noreply.github.com>
2024-05-20 12:24:21 -0500

    Further commenting and explanation of the TimedMode class.

--- TimedMode.java
+++ TimedMode.java
@@ -16,10 +16,18 @@ public class TimedMode extends Thread {
         answer = value;
     }
 
+    // Timer will begin once run() is called. Keeps track of current time
+    // and once a second has passed, will print the amount of time left and 
+    // adjust the "start" variable to a new starting point and continue onward
+    // until another second has passed.
     public void run() 
     {
         try {
+            
+            // start keeps track of starting time in nano seconds.
             long start = System.nanoTime();
+
+            // while the maximum amount of has not passed, keep counting down.
             while(timeLimit > 0) {
                 long possibleSecond = System.nanoTime();
                 if((possibleSecond - (long) 1e9) >= start) {
@@ -29,6 +37,7 @@ public class TimedMode extends Thread {
                 }
             }
 
+            // User has run out of time. Let them know and close the program.
             System.out.println("Timelimit Reached! The answer was " + answer);
             System.exit(0);
         }
