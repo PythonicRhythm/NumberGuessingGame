abecde194a2bf02fb8a121811ce5d3d54f291b62
Anthony Alvarez <101856161+PythonicRhythm@users.noreply.github.com>
2024-05-20 15:59:49 -0500

    Slight commenting, Progress stuck.

--- demo/src/test/java/GuessingTest.java
+++ demo/src/test/java/GuessingTest.java
@@ -1,18 +1,67 @@
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.IOException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import com.example.ClassicGuessing;
+import com.example.DifficultyGuessing;
+import com.example.TimedGuessing;
+
+// TESTS ARE INCOMPLETE
 
 public class GuessingTest {
     
+    // Making sure that the correct answer is within the given range
+    // set by difficulty or general settings.
     @Test
-    public void no_values_out_of_ranges() throws IOException {
+    public void no_values_out_of_ranges() {
         
-        String t1 = "3\n30\n10\n10\n10\n10\n10\n10\n10\n10\n10\n";
-        ClassicGuessing.run(t1);
-        assertTrue(ClassicGuessing.getAnswer() < 101 && ClassicGuessing.getAnswer() > 0);
-        assertFalse(ClassicGuessing.getAnswer() < 101 && ClassicGuessing.getAnswer() > 0);
+        // TESTING CLASSIC MODE
+        for(int i = 0; i < 100; i++) {
+            String t1 = "3\n0\n";
+            int answer = ClassicGuessing.run(t1);
+            Assertions.assertTrue(answer < 101 && answer > 0);
+        }
+
+        // TESTING DIFFICULTY MODE
+        // Easy Mode
+        for(int i = 0; i < 100; i++) {
+            String t1 = "1\n0\n";
+            int answer = DifficultyGuessing.run(t1);
+            Assertions.assertTrue(answer < 51 && answer > 0);
+
+        }
+        // Medium Mode
+        for(int i = 0; i < 100; i++) {
+            String t1 = "2\n0\n";
+            int answer = DifficultyGuessing.run(t1);
+            Assertions.assertTrue(answer < 101 && answer > 0);
+        }
+        // Hard Mode
+        for(int i = 0; i < 100; i++) {
+            String t1 = "3\n0\n";
+            int answer = DifficultyGuessing.run(t1);
+            Assertions.assertTrue(answer < 501 && answer > 0);
+        }
+
+        // TESTING TIMED MODE
+        // Easy Mode
+        for(int i = 0; i < 100; i++) {
+            String t1 = "1\n0\n";
+            int answer = TimedGuessing.run(t1);
+            Assertions.assertTrue(answer < 51 && answer > 0);
+
+        }
+        // Medium Mode
+        for(int i = 0; i < 100; i++) {
+            String t1 = "2\n0\n";
+            int answer = TimedGuessing.run(t1);
+            Assertions.assertTrue(answer < 101 && answer > 0);
+        }
+        // Hard Mode
+        for(int i = 0; i < 100; i++) {
+            String t1 = "3\n0\n";
+            int answer = TimedGuessing.run(t1);
+            Assertions.assertTrue(answer < 501 && answer > 0);
+        }
+
     }
 }
